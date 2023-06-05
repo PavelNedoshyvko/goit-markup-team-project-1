2e394c1c5922ba1a1516bc58100c7950915b76fc
Pavel Nedoshyvko <plight83@gmail.com>
2023-06-05 23:28:13 +0300

    Update modal.js

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,14 +1,39 @@
 (() => {
   const refs = {
-    openModalBtn1: document.querySelector("[data-modal-open-3]"),
-    closeModalBtn1: document.querySelector("[data-modal-close-3]"),
-    modal1: document.querySelector("[data-modal-3]"),
-	};
-
-  refs.openModalBtn1.addEventListener("click", toggleModal);
-  refs.closeModalBtn1.addEventListener("click", toggleModal);
-  
+    // two buttons one window
+    openBuynowModalBtn: document.querySelector('[data-buynow-modal-open-1]'),
+    openBuynowModalBtnSecond: document.querySelector(
+      '[data-buynow-modal-open-2]'
+    ),
+    closeBuynowModalBtn: document.querySelector('[data-buynow-modal-close]'),
+    modalBuynow: document.querySelector('[data-buynow-modal]'),
+    // review modal
+    openReviewModalBtn: document.querySelector('[data-review-modal-open]'),
+    closeReviewModalBtn: document.querySelector('[data-review-modal-close]'),
+    modalReview: document.querySelector('[data-review-modal]'),
+    // subscribe modal
+    openSubscribeModalBtn: document.querySelector(
+      '[data-subscribe-modal-open]'
+    ),
+    closeSubscribeModalBtn: document.querySelector(
+      '[data-subscribe-modal-close]'
+    ),
+    modalSubscribe: document.querySelector('[data-subscribe-modal]'),
+  };
+  refs.openBuynowModalBtn.addEventListener('click', toggleModal);
+  refs.openBuynowModalBtnSecond.addEventListener('click', toggleModal);
+  refs.closeBuynowModalBtn.addEventListener('click', toggleModal);
+  refs.openReviewModalBtn.addEventListener('click', toggleReviewModal);
+  refs.closeReviewModalBtn.addEventListener('click', toggleReviewModal);
+  refs.openSubscribeModalBtn.addEventListener('click', toggleSubscribeModal);
+  refs.closeSubscribeModalBtn.addEventListener('click', toggleSubscribeModal);
   function toggleModal() {
-    refs.modal1.classList.toggle("is-hidden");
+    refs.modalBuynow.classList.toggle('is-hidden');
   }
+  function toggleReviewModal() {
+    refs.modalReview.classList.toggle('is-hidden');
+  }
+   function toggleSubscribeModal() {
+     refs.modalSubscribe.classList.toggle('is-hidden');
+   }
 })();
